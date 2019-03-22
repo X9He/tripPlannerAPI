e86abe0dc85c8e21114d484b5c9b28b81c17c7a7
Evan He <evan.he@freshii.com>
2019-03-22 19:19:06 -0400

    can request from mapquest api now

--- app/controllers/gen_controller.rb
+++ app/controllers/gen_controller.rb
@@ -1,10 +1,10 @@
-require 'app/modules/GenerateTrip'
+require_relative '../modules/GenerateTrip'
 
 class GenController < ApplicationController
   # GET /trips/generate
   def generate
     puts 'in generate'
     # puts JSON.parse(request.body.string)['locations']
-    GenerateTrip.generate_trip(JSON.parse(request.body.string)['locations'])
+    GenerateTrip.generate_trip(['derp', 'derp'])
   end
 end
--- app/modules/GenerateTrip.rb
+++ app/modules/GenerateTrip.rb
@@ -1,7 +1,15 @@
+require 'rubygems'
+require 'http'
+
 module GenerateTrip
   def self.generate_trip(locations)
+    key = 'GFCgDDLXoK4wA1zqapmAXlVAR3MQIY6k'
+    res = HTTP.get("http://www.mapquestapi.com/directions/v2/optimizedroute?key=#{key}&json={'locations':['Denver,CO','Westminster,CO','Boulder,CO']}")
+    puts 'stuff'
+    res = JSON.parse(res.to_s)
+    puts res['route']['realTime']
     locations.each do |i|
-      puts i
+      puts 'why????'
     end
   end
 end
