b78c52c23f621dfa1b228d0a114e40817a07c462
Evan He <hexianliang9@gmail.com>
2019-03-31 23:18:47 -0400

    simulated annealing for TSP problem

--- app/modules/GenerateTrip.rb
+++ app/modules/GenerateTrip.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'http'
 require_relative 'MST'
+require_relative '../modules/MST'
 
 module GenerateTrip
   def self.generate_trip(locations)
@@ -9,34 +10,35 @@ module GenerateTrip
     puts "derp saucea"
     puts tourist_locations
     tourist_locations.push(starting_address)
-    # lunchLocation = locations.lunch
-    # dinnerLocation = locations.dinner
-    locations_hash = Hash.new()
+    routes_hash = Hash.new()
     key = 'GFCgDDLXoK4wA1zqapmAXlVAR3MQIY6k'
     puts 'stuff'
     array_of_routes = []
     tourist_locations.each_with_index do |loc1, i|
       tourist_locations.each_with_index do |loc2, j|
-        next unless j > i && j == tourist_locations.length - 1
+        next unless j > i
 
         puts loc1, loc2
         puts i, j
         tourist_locations_i_escaped = tourist_locations[i].gsub!(/\s/, '%20')
         tourist_locations_j_escaped = tourist_locations[j].gsub!(/\s/, '%20')
-        request_url = "http://www.mapquestapi.com/directions/v2/optimizedroute?key=#{key}&json={\"locations\":[\"#{tourist_locations_i_escaped}\", \"#{tourist_locations_j_escaped}\"]}"
+        # request_url = "http://www.mapquestapi.com/directions/v2/optimizedroute?key=#{key}&json={\"locations\":[\"#{tourist_locations_i_escaped}\", \"#{tourist_locations_j_escaped}\"]}"
 
-        puts request_url
+        # puts request_url
         # res = HTTP.get(request_url)
         res = HTTP.get("http://www.mapquestapi.com/directions/v2/optimizedroute",
                        :params => {:key => key,
                                    json: "{locations:[#{tourist_locations_i_escaped}, #{tourist_locations_j_escaped}]}"})
         res = JSON.parse(res.to_s)
         puts res
-        # puts res['route']['realTime']
-        # array_of_routes.push(res)
+        puts res['route']['realTime']
+        array_of_routes.push(res)
+        routes_hash[loc1 + loc2] = res['route']['realTime']
         # locations_hash[tourist_locations[i] + ',' + tourist_locations[j]] = res
       end
     end
+    tourist_locations.pop
+    MST.do_MST(routes_hash, tourist_locations, starting_address)
       # puts locations_hash
   end
 end
--- app/modules/MST.rb
+++ app/modules/MST.rb
@@ -1,5 +1,34 @@
 module MST
-  def self.do_MST(list_of_routes)
+  def self.get_travel_time(hash_of_routes, route, starting_address)
+    start_cost = hash_of_routes[starting_address + route[0]]
+    end_cost = hash_of_routes[route[0] + starting_address]
+    total_cost = start_cost + end_cost
+    route.each_with_index do |loc, pos|
+      if pos < route.length - 1
+        total_cost += hash_of_routes[loc + route[pos + 1]]
+      end
+    end
+    total_cost
+  end
 
+  def self.do_MST(hash_of_routes, tourist_locations, starting_address)
+    temperature = 15_000
+    while temperature > 0.1
+      new_locations = tourist_locations
+      new_locations.shuffle
+      cur_cost = get_travel_time(hash_of_routes, tourist_locations, starting_address)
+      new_cost = get_travel_time(hash_of_routes, new_locations, starting_address)
+      delta_e = cur_cost - new_cost
+      if delta_e.positive?
+        tourist_locations = new_locations
+      else
+        prob = Math.exp(delta_e/temperature)
+        tourist_locations = new_locations if rand(1) <= prob
+      end
+      log_x = Math.log(temperature, 1.5)
+      log_x -= 0.0003
+      temperature = 1.5**log_x
+      end
+    tourist_locations
   end
 end
